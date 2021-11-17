b4f756c0e059f9583ed03c8512774f7f09705199
Lyrenyx Maxima <haishenrong@gmail.com>
2021-11-17 01:09:25 -0500

    Full LSH results

--- knn_lsh.py
+++ knn_lsh.py
@@ -1,6 +1,8 @@
 from user import User
 from methods import read_ratings, read_movies, shuffle_x
+from config.mysqlconnection import connectToMySQL
 from lsh import min_hash, hash_buckets
+import json
 import sys
 
 sys.stdout.reconfigure(encoding='utf-8')
@@ -8,18 +10,36 @@ sys.stdout.reconfigure(encoding='utf-8')
 TOTAL_USERS = 162541
 TOTAL_MOVIES = 62423
 
-user_ratings = read_ratings(75000)
-movies = read_movies()
+# user_ratings = read_ratings(162541)
 # for row in user_ratings:
 #     print(row.userId)
 #     print(row.ratings)
 
+# movies = read_movies()
 # for row in movies:
 #     print(row)
 # Creates hundred_shuffles.csv
 # shuffle_x(movies, 100)
-hash_signature = min_hash(user_ratings, 10)
+
+# Hash signature
+# hash_signature = min_hash(user_ratings, 24)
 # for key in hash_signature:
 #     print(key, hash_signature[key])
-hash_buckets = convert_bucket(hash_signature, 3)
 
+# Bucket making
+# hash_buckets = hash_buckets(hash_signature, 2)
+# for key in hash_buckets:
+#     print(key, hash_buckets[key])
+# print(len(hash_buckets))
+
+# Write buckets
+# with open('full_result.json', 'w') as fp:
+#   json.dump(hash_buckets, fp)
+
+
+with open('full_result.json', 'r') as res:
+    hash_buckets = json.load(res)
+
+for key in hash_buckets:
+    print(key, hash_buckets[key])
+print(len(hash_buckets))
--- lsh.py
+++ lsh.py
@@ -1,8 +1,9 @@
 import io, csv
+from datetime import datetime
 
 def min_hash(userList, times=1):
     minHashSignature = {}
-    file = io.open('hundred_shuffles.csv', mode="r", encoding="utf-8", errors="ignore")
+    file = io.open('ml-25m/hundred_shuffles.csv', mode="r", encoding="utf-8", errors="ignore")
     csvreader = csv.reader(file)
     movieList = next(csvreader)
     for user in userList:
@@ -12,10 +13,19 @@ def min_hash(userList, times=1):
             j = 0
             while j<len(movieList):
                 if int(movieList[j]) in user.ratings and user.ratings[int(movieList[j])] >= 4.0:
-                    minHashSignature[user.userId].append(j)
+                    minHashSignature[user.userId].append(movieList[j])
                     break
-                j+=1
+                j += 1
         movieList = next(csvreader)
     return minHashSignature
 
-def hash_buckets(hash_signature, bandwidth = 3)
+def hash_buckets(hash_signature, bandwidth = 3):
+    buckets = {}
+    for key, value in hash_signature.items():
+        for i in range(0, len(value), bandwidth):
+            bucketKey = ','.join(value[i:i+bandwidth])
+            if bucketKey in buckets and key not in buckets[bucketKey]:
+                buckets[bucketKey].append(key)
+            else:
+                buckets[bucketKey]=[key]
+    return buckets
